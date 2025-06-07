6ef3a6b41f8b7e0f702676ba7913351f5c54e10d
mikorozek <mikolajrozek0@gmail.com>
2025-06-07 11:21:27 +0200

    Added new necessary fields to config class

--- src/config.py
+++ src/config.py
@@ -11,12 +11,15 @@ class Config:
     WEIGHT_DECAY = 0.01
 
     OUTPUT_DIR = "results"
+    PLOTS_DIR = "plots"
     BEST_MODEL_PATH = "/models"
     DATASET_NAMES = ["allegro/klej-polemo2-in"]
     DATASET_PATHS = {
         "train": "data/train_tokenized",
         "validation": "data/val_tokenized",
+        "test": "data/test_tokenized",
     }
+    MODEL_CHECKPOINT_PATH = "results/checkpoint-600"
     REQUIRED_COLUMNS = ["title", "text", "label"]
 
     WANDB_PROJECT = "sentiment-classification"
